b0bf7f36c10f291256dcf95a0831e8168d080664
Drivas04 <drivasb@unbosque.edu.co>
2025-03-13 20:20:40 -0500

    creacion del repositorio del proyecto final de base de datos 2

---
+++ BACKEND_PAYROLL/src/main/java/co/edu/unbosque/backend_payroll/BackendPayrollApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.backend_payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPayrollApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendPayrollApplication.class, args);
+    }
+
+}
---
+++ BACKEND_PAYROLL/src/test/java/co/edu/unbosque/backend_payroll/BackendPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.backend_payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPayrollApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
