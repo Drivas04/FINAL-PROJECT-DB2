2b6d1c16311dc4c0c4d4465bbff283c5af8558eb
Drivas04 <drivasb@unbosque.edu.co>
2025-03-16 19:31:37 -0500

    Creacion de modulos

--- FRONTEND_PAYROLL/src/components/Header.jsx
+++ FRONTEND_PAYROLL/src/components/Header.jsx
@@ -3,7 +3,12 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='flex flex-wrap gap-4 mx- auto'>
+      <div>Gestion de empleados</div>
+      <div>Liquidacion de seguro social</div>
+      <div>Liquidacion de nomina</div>
+      <div>Liquidacion de cesantias</div>
+    </header>
   )
 }
 
