79194725f4ea7aab69a060f3609b6f1d43f10abe
Drivas04 <drivasb@unbosque.edu.co>
2025-04-17 22:14:59 -0500

    Creacion de endpoints para departamentos para probar su integracion con el Frontend

--- BACKEND_PAYROLL/src/main/java/co/edu/unbosque/backend_payroll/controller/DepartamentoController.java
+++ BACKEND_PAYROLL/src/main/java/co/edu/unbosque/backend_payroll/controller/DepartamentoController.java
@@ -6,12 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/departamentos")
+@CrossOrigin(origins = "http://localhost:5173")
 public class DepartamentoController {
 
     @Autowired
     private DepartamentoService departamentoService;
+
+    @GetMapping("/")
+    public List<Departamento> getAllDepartamentos() {
+        return departamentoService.getAll();
+    }
 
     @PutMapping("/editar-departamento/{id_department}")
     public ResponseEntity<Void> editarDepartamento(@PathVariable Short id_department, String new_name) {
--- BACKEND_PAYROLL/src/main/java/co/edu/unbosque/backend_payroll/service/DepartamentoService.java
+++ BACKEND_PAYROLL/src/main/java/co/edu/unbosque/backend_payroll/service/DepartamentoService.java
@@ -1,14 +1,21 @@
 package co.edu.unbosque.backend_payroll.service;
 
+import co.edu.unbosque.backend_payroll.entity.Departamento;
 import co.edu.unbosque.backend_payroll.repository.DepartamentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DepartamentoService {
 
     @Autowired
     private DepartamentoRepository departamentoRepository;
+
+    public List<Departamento> getAll() {
+        return departamentoRepository.findAll();
+    }
 
     public void updateDepartment(Short id_department, String new_name){
         departamentoRepository.updateDepartment(id_department, new_name);
